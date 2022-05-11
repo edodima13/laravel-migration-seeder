cbe0fabd67a8b8ca5153756c82e898e41de6518b
edodima13 <edodima13@libero.it>
2022-05-11 18:15:07 +0200

    avanzamento e fine fase 2

--- database/seeds/TrainTableSeeder.php
+++ database/seeds/TrainTableSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Train;
 use Illuminate\Database\Seeder;
-use Faker\Generator as Faker;
+use Faker\Generator as faker;
 
 class TrainTableSeeder extends Seeder
 {
@@ -12,6 +13,20 @@ class TrainTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 100; $i++) { 
+            $train = new Train();
+
+            $train-> company = $faker->company();
+            $train-> arrival_station = $faker->city();
+            $train-> departure_station = $faker->city();
+            $train-> departure = $faker->dateTimeBetween('-1 week','+1 week');
+            $train-> arrival = $faker->dateTimeBetween('-1 week','+1 week');
+            $train-> code = $faker->bothify('??-######');
+            $train-> carriages_number = $faker->numberBetween(3,20);
+            $train-> in_time = $faker->boolean();
+            $train-> deleted = $faker->numberBetween(1,0);
+
+            $train->save();
+        }
     }
 }
